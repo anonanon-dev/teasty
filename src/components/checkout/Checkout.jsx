cf08e88e1240e0cae7dde968db38a941da7c4165
samaateff <71303025+samaateff@users.noreply.github.com>
2024-09-06 12:49:54 +0300

    xx

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -21,6 +21,7 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
 // import { Box } from '@mui/material';
 
 // <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
